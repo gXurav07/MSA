0c6100a875d8edee5b8f9869ffaca6f85c377e92
Gaurav Malakar <varuag2002@gmail.com>
2022-07-30 22:10:20 +0530

    added files required for deploying

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: /msa/manage.py
--- msa/shop/forms.py
+++ msa/shop/forms.py
@@ -91,3 +91,4 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+ 
